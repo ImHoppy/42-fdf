f394d49614f54cc1fa0ba13351a64c078fe34140
Marius BRAETS <mbraets@student.42.fr>
2022-03-02 18:48:11 +0100

    KeyRelease to KeyPress

--- minilibx/mlx_key_hook.c
+++ minilibx/mlx_key_hook.c
@@ -16,7 +16,7 @@
 
 int		mlx_key_hook(t_win_list *win,int (*funct)(),void *param)
 {
-  win->hooks[KeyRelease].hook = funct;
-  win->hooks[KeyRelease].param = param;
-  win->hooks[KeyRelease].mask = KeyReleaseMask;
+  win->hooks[KeyPress].hook = funct;
+  win->hooks[KeyPress].param = param;
+  win->hooks[KeyPress].mask = KeyPressMask;
 }
